105686ca87681df0ba264ce44ca18adee617955b
gyuch-an02 <gyuch.an02@kaist.ac.kr>
2024-07-20 15:02:03 +0900

    Feat: Colorpicker, Single Pixel

--- madcamp4_frontend/src/pages/Page2.tsx
+++ madcamp4_frontend/src/pages/Page2.tsx
@@ -4,7 +4,7 @@ import CRDTCanvas from '../components/CRDTCanvas';
 const Page2: React.FC = () => {
   return (
     <div>
-      <h1>CRDT Canvas</h1>
+      <h1>CRDT Canvas!</h1>
       <CRDTCanvas />
     </div>
   );
